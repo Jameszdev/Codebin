eabb8bf4d4227e312b39c9bfb3f4bd405a1df952
OrangeBeatle123 <jamesammulligan@gmail.com>
2022-03-18 17:27:52 +0000

    Added embeds on snippets

--- server.js
+++ server.js
@@ -28,10 +28,36 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     const code = `
-    
-Welcome to Codebin!
+# Codebin
 
-A Simple and Easy way to share your code snippets with anyone!
+A simple and easy to use code snippet sharer. 
+
+Source Code: [Here](https://git.pogr.cc/root/codebin)
+Example: https://codebin.jamesz.dev/14pgcjk
+
+## Installation
+
+You can either use the online version or self host your own with your own domain and control your own data!
+
+All you need is [Node.Js](https://nodejs.org/en/) and [MongoDB](https://www.mongodb.com/).
+
+
+#Clone this repo
+git clone https://git.pogr.cc/root/codebin.git
+#Install Dependencies 
+npm i
+#Edit environment variables shown below.
+
+## Environment Variables
+
+#Create an .env file and enter:
+PORT=3000 # whatever port you would like the webserver to run on
+
+MONGO_URI= #Your MongoDB URI
+
+## Contributing
+Feel free to suggest new features by opening Issues or Pull Requests!
+    
     `
 
     res.render('code', { code, language: "plaintext" })
